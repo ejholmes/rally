cff9e1eb0a4f64dd0db6d1e47209e19d36270c3c
Eric J. Holmes <eric@ejholmes.net>
2014-03-06 13:26:38 -0800

    More docs.

--- lib/rally.rb
+++ lib/rally.rb
@@ -13,27 +13,38 @@ module Rally
   autoload :Runner
 
   class << self
+
+    # Public: Logger instance to use.
+    # 
+    # Returns a Logger instance.
     def logger
       @logger ||= Logger.new(STDOUT)
     end
 
+    # Public: Registers a provider to be used by the Runner.
+    #
+    # klass - A class that inherits from Rally::Provider.
+    #
+    # Returns nothing.
     def register_provider(klass)
       providers << klass
     end
 
+    # Public: A list of all the registered Providers.
+    #
+    # Returns an Array of Providers.
     def providers
       @providers ||= []
     end
 
-    def setup(name, options = {})
-    end
-
-    def eval(&block)
-      evaluator.eval(&block)
+    # Public: Evals some code in the Rally::Runner.
+    def eval(*args, &block)
+      evaluator.eval(*args, &block)
     end
 
   private
 
+  # Internal: A Rally::Runner instance to evaluate code inside.
     def evaluator
       @evaluator ||= Runner.new
     end
