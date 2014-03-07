26667e2711d135f74415b52e7900add6c940a93f
Eric J. Holmes <eric@ejholmes.net>
2014-03-06 16:37:12 -0800

    Ensure an app was created.

--- features/step_definitions/providers/heroku_steps.rb
+++ features/step_definitions/providers/heroku_steps.rb
@@ -1,2 +1,5 @@
 Then(/^I should have a heroku app called "(.*?)"$/) do |app|
+  provider = Rally::Providers::Heroku.new
+  response = provider.connection.get "/apps/#{app}"
+  expect(response.status).to eq 200
 end
