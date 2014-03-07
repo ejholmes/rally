05ff1d90dc6dc318c9877240b52f1c6706698fc8
Eric J. Holmes <eric@ejholmes.net>
2014-03-06 16:46:32 -0800

    heroku app.

--- features/step_definitions/providers/heroku_steps.rb
+++ features/step_definitions/providers/heroku_steps.rb
@@ -4,7 +4,7 @@ Then(/^I should have a heroku app called "(.*?)"$/) do |app|
   expect(response.status).to eq 200
 end
 
-Then(/^I should have a drain with the url "(.*?)" on the app called "(.*?)"$/) do |url, app|
+Then(/^I should have a drain with the url "(.*?)" on the heroku app called "(.*?)"$/) do |url, app|
   provider = Rally::Providers::Heroku.new
   response = provider.connection.get "/apps/#{app}/log-drains"
   expect(response.body.find { |drain| drain['url'] == url }['url']).to eq url
